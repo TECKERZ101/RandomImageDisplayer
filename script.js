94cb628590bc0f7c53e8536403fa2630c50ab0a2
teckerz101 <isaacclifton@hotmail.com>
2024-06-19 20:56:16 +0100

    Changes
    Signed-off-by: teckerz101 <isaacclifton@hotmail.com>

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 window.onload = function() {
-    selectRandomImage();
-};
+    getRandomImage();
+}
+
+
+
+function getRandomImage() {
+    fetch('C:\\Users\\isaac\\Downloads\\wypher') // Replate if neccisarry
+        .then(response => response.text())
+        .then(html => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(html, 'text/html');
+            const links = Array.from(doc.querySelectorAll('a'));
+            const fileLinks = links.filter(link => link.href.endsWith('.txt')); // Adjust the filter based on the file types you're interested in
+            const filenames = fileLinks.map(link => link.textContent);
+            return filenames
+    })
+    .catch(error => console.error('Error fetching directory:', error));
+
+    const images = filenames
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
